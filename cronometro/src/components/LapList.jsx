1960e1e55d42aa9588740eaf3dee3b94b1d2df9f
elissandroa <elissandroa@hotmail.com>
2024-05-30 14:36:22 -0300

    Setup Timer Project

---
+++ cronometro/src/components/LapList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LapList = () => {
+  return (
+    <div>LapList</div>
+  )
+}
+
+export default LapList
---
+++ cronometro/src/components/TimerControls.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TimerControls = () => {
+  return (
+    <div>TimerControls</div>
+  )
+}
+
+export default TimerControls
