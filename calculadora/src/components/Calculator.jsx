81fe51f629199c47f8dc5d99708325dba1132c1c
elissandroa <elissandroa@hotmail.com>
2024-05-30 12:18:49 -0300

    Component + CSS

--- calculadora/src/components/Calculator.jsx
+++ calculadora/src/components/Calculator.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import './calculator.css';
 
 const Calculator = () => {
-  return (
-    <div className="calculator">
-        <div className="complete-operation">3 + 3 = 6</div>
-        <div className="display">6</div>
-        <div className="buttons"></div>
-        <button>=</button>
-    </div>
-  )
+    const keyPadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const operations = ["+", "-", "*", "/"];
+
+    return (
+        <div className="calculator">
+            <div className="complete-operation"></div>
+            <div className="display"></div>
+            <div className="buttons">
+                <button>AC</button>
+                {keyPadNumbers.map((num) => (
+                    <button key={num}>{num}</button>
+                ))}
+                {
+                    operations.map((operation) => (
+                        <button key={operation}>{operation}</button>
+                    ))
+                }
+                <button>=</button>
+            </div>
+
+        </div>
+    )
 }
 
 export default Calculator
