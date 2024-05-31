9b216b806991dcac0b868cee2ccc76bf170721c4
elissandroa <elissandroa@hotmail.com>
2024-05-31 15:18:44 -0300

    Fetching weather data from the API

--- clima/src/App.jsx
+++ clima/src/App.jsx
@@ -1,23 +1,33 @@
 import "./AppStyled.js";
 
 import { useEffect, useState } from "react";
-import axios from 'axios'/
+import axios from 'axios';
 
 import Busca from "./components/Busca.jsx";
 import ClimaAtual from "./components/ClimaAtual.jsx";
 import Previsao from "./components/Previsao.jsx";
 import { Titulo } from "./AppStyled.js";
 
+const apiKey = import.meta.env.VITE_API_KEY || "";
 
 function App() {
   const [cidade, setCidade] = useState("");
   const [clima, setClima] = useState(null);
   const [previsao, setPrevisao] = useState([]);
 
+  const buscarClima = async () => {
+    try {
+      const respostaClima = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}`)
+      setClima(respostaClima.data);
+    } catch (error) {
+      console.log("Erro ao buscar clima: ", error);
+    }
+  }
+
   return (
     <div>
-      <Titulo>Condições climáticas</Titulo> 
-      <Busca />
+      <Titulo>Condições climáticas</Titulo>
+      <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima} />
       <ClimaAtual />
       <Previsao />
     </div>
--- clima/src/components/Busca.jsx
+++ clima/src/components/Busca.jsx
@@ -1,9 +1,9 @@
 
-const Busca = () => {
+const Busca = ({cidade, setCidade, buscarClima}) => {
   return (
     <div>
-        <input type="text" />
-        <button>Buscar</button>
+        <input type="text" value={cidade} onChange={(e) => setCidade(e.target.value)} placeholder="Digite uma cidade"/>
+        <button onClick={buscarClima}>Buscar</button>
     </div>
   )
 }
