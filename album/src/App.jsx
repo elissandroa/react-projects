75c7ef0eabd44735143cbc568c5cef9eeb2c8cb3
elissandroa <elissandroa@hotmail.com>
2024-06-04 13:22:07 -0300

    Configure API Key

--- album/src/App.jsx
+++ album/src/App.jsx
@@ -3,15 +3,33 @@ import FotoAmpliada from './componentes/FotoAmpliada'
 import FotoList from './componentes/FotoList'
 import Searchbar from './componentes/Searchbar'
 import { useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
 
-  
+  const [query, setQuery] = useState("");
+  const [category, setCategory] = useState("");
+
+  const fetchData = async (query, category) => {
+    
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+    const response = await axios.get("https://api.unsplash.com/photos/random", {
+      params: {
+        client_id: apiKey
+      },
+    });
+    console.log(response);
+  }
+
+  useEffect(() => {
+    fetchData(query, category);
+  }, [])
+
   return (
     <div className='container'>
-    <Searchbar />
-    <FotoList />
-    <FotoAmpliada />
+      <Searchbar />
+      <FotoList />
+      <FotoAmpliada />
     </div>
   )
 }
