4dd195d9123dde74cd90a26e1694d529e4569916
elissandroa <elissandroa@hotmail.com>
2024-06-05 10:24:40 -0300

    Converting markdown in html

--- markdown_viewer/src/App.jsx
+++ markdown_viewer/src/App.jsx
@@ -1,12 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 import { Toolbar } from "./components/Toolbar";
+import { marked } from "marked";
+
 
 function App() {
+  const [text, setText] = useState("# Olá eu sou feito com markdown");
+  const textAreaRef = useRef(null);
+
+  const renderText = () => {
+    return {__html: marked(text)}
+  }
+
   return (
     <div className="container">
       <Toolbar />
-      <textarea></textarea>
-      <div></div>
+      <textarea
+        ref={textAreaRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <div dangerouslySetInnerHTML={renderText()}></div>
     </div>
   );
 }
--- markdown_viewer/src/components/Toolbar/index.jsx
+++ markdown_viewer/src/components/Toolbar/index.jsx
@@ -1,7 +1,16 @@
-import './styles.css';
+import "./styles.css";
 
 export const Toolbar = () => {
   return (
-    <div>Toolbar</div>
-  )
-}
+    <div className="toolbar">
+      <button>h1</button>
+      <button>h2</button>
+      <button>bold</button>
+      <button>Italico</button>
+      <button>Link</button>
+      <button>Code block</button>
+      <button>List item</button>
+      <button>Horizontal line</button>
+    </div>
+  );
+};
