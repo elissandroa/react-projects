6192dd139f10628ca3ab1ac1b69a4c12e7d42032
elissandroa <elissandroa@hotmail.com>
2024-06-06 09:12:23 -0300

    Structuring the board

--- jogo_da_velha/src/components/Board.jsx
+++ jogo_da_velha/src/components/Board.jsx
@@ -2,9 +2,34 @@ import React, { useEffect, useState } from 'react'
 import { Square } from './Square'
 
 export const Board = () => {
+    const [isNext, setIsNext] = useState(true);
+    const winner = false;
+
+
+
     return (
         <div>
+            <div className="status">
+                Status:{" "}
+                {winner ? (<p className='winner'>O vencedor é {winner}</p>
+            ) : (`Próximo a jogar: ${isNext ? "X" : "O"}`)}
+            </div>
+            <div className="board-row">
             <Square />
+            <Square />
+            <Square />
+            </div>
+            <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+            </div>
+            <div className="board-row">
+            <Square />
+            <Square />
+            <Square />
+            </div>
+            <button className='reset-button'>Reiniciar jogo</button>
         </div>
     )
 }
--- jogo_da_velha/src/components/Square.jsx
+++ jogo_da_velha/src/components/Square.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Square = () => {
   return (
-    <div>Square</div>
+    <button className='square'></button>
   )
 }
