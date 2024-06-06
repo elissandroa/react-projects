fcbe9458b9267f814d0ff444a7cde460ad85d867
elissandroa <elissandroa@hotmail.com>
2024-06-06 16:49:48 -0300

    Filtering tasks and localStorage implementing - Project finished

--- todo_redux/src/components/TodoList.jsx
+++ todo_redux/src/components/TodoList.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { toggleTodo, removeTodo } from '../slices/todoSlice';
+import { toggleTodo, removeTodo, filtersTodo } from '../slices/todoSlice';
 
 
 export const TodoList = () => {
     const { list, filter } = useSelector((state) => state.todos);
     const dispatch = useDispatch();
 
+    const filteredList = list.filter((todo) => {
+        if(filter === "all") return true;
+        if(filter === "completed") return todo.completed;
+        if(filter === "incomplete") return !todo.completed;
+        return true;
+    })
+
     return (
         <div>
-            <button>Todas</button>
-            <button>Completas</button>
-            <button>Incompetas</button>
+            <button onClick={() => dispatch(filtersTodo("all"))}>Todas</button>
+            <button onClick={() => dispatch(filtersTodo("completed"))}>Completas</button>
+            <button onClick={() => dispatch(filtersTodo("incomplete"))}>Incompetas</button>
             <ul>
-                {list.map((todo) => (
+                {filteredList.map((todo) => (
                     <li key={todo.id}>
                         <span onClick={() => dispatch(toggleTodo(todo.id))} className={todo.completed ? "line-through" : null}>{todo.text}</span>
                         <button onClick={() => dispatch(removeTodo(todo.id))}>Remover</button>
--- todo_redux/src/slices/todoSlice.js
+++ todo_redux/src/slices/todoSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
 
-
+const lista = JSON.parse(localStorage.getItem("to-dos"));
 export const todoSlice = createSlice({
     name: "todos",
     initialState: {
-        list: [],
+        list: lista,
         filter: "all",
     }, reducers: {
         addTodo: (state, action) => {
@@ -12,21 +13,28 @@ export const todoSlice = createSlice({
                 id: new Date().toISOString(),
                 text: action.payload,
                 completed: false,
-            })
+            }),
+            localStorage.setItem("to-dos", (JSON.stringify(state.list)));
         },
         toggleTodo: (state, action) => {
             const todo = state.list.find((todo) => todo.id === action.payload);
             if (todo) {
                 todo.completed = !todo.completed;
+                localStorage.setItem("to-dos", (JSON.stringify(state.list)));
             }
         },
         removeTodo: (state, action) => {
-           state.list = state.list.filter((todo) => todo.id != action.payload);
-        }
+            state.list = state.list.filter((todo) => todo.id != action.payload);
+            localStorage.setItem("to-dos", (JSON.stringify(state.list)));
+           
+        },
+        filtersTodo: (state, action) => {
+            state.filter = action.payload;
+        },
     }
 
 });
 
-export const { addTodo, toggleTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, toggleTodo, removeTodo, filtersTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
