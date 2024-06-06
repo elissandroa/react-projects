564a4f0300549b7daf1eb3515491df8ba39e3812
elissandroa <elissandroa@hotmail.com>
2024-06-06 08:59:07 -0300

    Starting Jogo da Velha

---
+++ jogo_da_velha/src/components/Board.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react'
+import { Square } from './Square'
+
+export const Board = () => {
+    return (
+        <div>
+            <Square />
+        </div>
+    )
+}
