0f86cd29614a0329a8d215b9381b9deef89d3616
elissandroa <elissandroa@hotmail.com>
2024-06-07 14:27:42 -0300

    Starting project - Calculadora de Impostos

---
+++ calculadora_imposto/src/App.jsx
@@ -0,0 +1,14 @@
+import { TaxForm } from "./components/TaxForm"
+import { TaxReport } from "./components/TaxReport"
+
+function App() {
+
+  return (
+  <div>
+    <TaxForm />
+    <TaxReport />
+  </div>
+  )
+}
+
+export default App
---
+++ calculadora_imposto/src/components/TaxForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TaxForm = () => {
+  return (
+    <div>TaxForm</div>
+  )
+}
