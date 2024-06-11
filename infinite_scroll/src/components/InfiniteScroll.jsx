812db1e006c2c5fa918d23a8c54967b69392aa5c
elissandroa <elissandroa@hotmail.com>
2024-06-11 16:01:45 -0300

    Loading Posts

--- infinite_scroll/src/components/InfiniteScroll.jsx
+++ infinite_scroll/src/components/InfiniteScroll.jsx
@@ -5,16 +5,52 @@ import axios from 'axios'
 
 
 export const InfiniteScroll = () => {
-    
-  return (
-    <div>
-        <h1>Infinite Scroll</h1>
-        <ul>
-            <li>
-                <h3>Título</h3>
-                <p>Post</p>
-            </li>
-        </ul>
-    </div>
-  )
+    const [post, setPost] = useState([]);
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasMoreDate, setHasMoreData] = useState(true);
+
+
+    const fetchPosts = async () => {
+        if (isLoading) return;
+
+        setIsLoading(true);
+
+        try {
+            const response = await axios.get(
+                `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
+            );
+            if (response.data.length > 0) {
+                setPost((prevPost) => [...prevPost, ...response.data]);
+            } else {
+                setHasMoreData(false);
+            }
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, [page]);
+
+    return (
+        <div>
+            <h1>Infinite Scroll</h1>
+            <ul>
+                {
+                    post.map((post) => (
+                        <li key={post.id}>
+                            <h3>{post.title}</h3>
+                            <p>{post.body}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+            {isLoading && <p>Carregando mais posts...</p>}
+        </div>
+    )
 }
