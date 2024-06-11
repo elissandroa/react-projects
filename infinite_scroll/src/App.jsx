85b151cc13a726b7f51e87f39dcf55fca3f721f5
elissandroa <elissandroa@hotmail.com>
2024-06-11 15:40:29 -0300

    Infinit Scroll Project

---
+++ infinite_scroll/src/App.jsx
@@ -0,0 +1,13 @@
+import { InfiniteScroll } from './components/InfiniteScroll'
+
+function App() {
+
+
+  return (
+   <div className="App">
+    <InfiniteScroll />
+   </div>
+  )
+}
+
+export default App
---
+++ infinite_scroll/src/components/InfiniteScroll.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
+
+export const InfiniteScroll = () => {
+    
+  return (
+    <div>
+        <h1>Infinite Scroll</h1>
+        <ul>
+            <li>
+                <h3>Título</h3>
+                <p>Post</p>
+            </li>
+        </ul>
+    </div>
+  )
+}
