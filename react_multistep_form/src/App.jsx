63b5de07518dc32c0ed4d93d5ea5e01c79ab8b7e
elissandroa <elissandroa@hotmail.com>
2024-06-19 10:40:50 -0300

    Step change functionality

--- react_multistep_form/src/App.jsx
+++ react_multistep_form/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { FiSend } from 'react-icons/fi'
 import { UserForm } from './components/UserForm'
 import { ReviewForm } from './components/ReviewForm'
 import { Thanks } from './components/Thanks'
@@ -13,7 +14,10 @@ function App() {
     <Thanks />
   ]
 
-  const { currentStep, currentComponent } = useForm(formComponents);
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents);
+
+
   return (
     <div className='app'>
       <div className="header">
@@ -25,18 +29,26 @@ function App() {
       </div>
       <div className="form-container">
         <p>Etapas</p>
-        <form>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">
             {currentComponent}
           </div>
           <div className="actions">
-            <button type="button">
-              <GrFormPrevious />
-              <span>Voltar</span></button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {
+              !isFirstStep && <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span></button>
+            }
+            {
+              !isLastStep ? (<button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>) :
+                (<button type="button">
+                  <span>Enviar</span>
+                  <FiSend />
+                </button>)
+            }
           </div>
         </form>
       </div>
