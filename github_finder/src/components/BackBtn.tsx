547483b0084c1f0b0c11963a7f1bf0e2bef183a1
elissandroa <elissandroa@hotmail.com>
2024-07-22 19:59:25 -0300

    Repository component

---
+++ github_finder/src/components/BackBtn.tsx
@@ -0,0 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
+
+export const BackBtn = () => {
+    const navigate = useNavigate();
+  return (
+    <>
+    <button onClick={() => navigate(-1) }>Voltar</button>
+    </>
+  )
+}
