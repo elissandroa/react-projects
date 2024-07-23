2bedddcc6fc737757bbb68cd3814685bc5c24a72
elissandroa <elissandroa@hotmail.com>
2024-07-23 10:16:31 -0300

    Initial Setup

---
+++ react-hook-form-com-ts/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Form } from './components/Form'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-bold text-3xl'>React Hook Form com Typescript</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App
