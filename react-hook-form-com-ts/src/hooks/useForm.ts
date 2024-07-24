981221d26d51673d55d2bc95a7e260dd6f2838dd
elissandroa <elissandroa@hotmail.com>
2024-07-24 11:09:22 -0300

    useForm

---
+++ react-hook-form-com-ts/src/hooks/useForm.ts
@@ -0,0 +1,13 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from 'react-hook-form';
+import { formSchema, FormSchema } from "../schema/formSchema";
+
+
+export const useForms = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<FormSchema>({
+        resolver: zodResolver(formSchema),
+
+    });
+
+    return { register, handleSubmit, errors };
+}
