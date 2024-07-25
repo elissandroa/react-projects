875865f71d56b43096ce164a333f66f59f406f61
elissandroa <elissandroa@hotmail.com>
2024-07-25 16:56:15 -0300

    Generating cards and reordering them randomly

--- jogo_da_memoria/src/components/Game.jsx
+++ jogo_da_memoria/src/components/Game.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Board } from './Board'
 
 export const Game = () => {
+    const shufleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    const generateCards = () => {
+
+        const values = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+        const cards = values.map((value) => ({
+            value,
+            isFlipped: false,
+        }));
+        const duplicateCards = cards.concat([...cards]).map((card, index) => ({ ...card, index }));
+        console.log(shufleArray(duplicateCards));
+
+    }
+
+    generateCards();
+    const [cards, setCards] = useState(generateCards());
+    const [flippedCards, setFlippedCards] = useState([]);
+    const [chances, setChances] = useState(6);
+
     return (
         <div className='game'>
             <h3>Game</h3>
