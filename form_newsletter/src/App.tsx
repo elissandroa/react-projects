bfe15941a44279ba32bda4d30ef987629daee12b
elissandroa <elissandroa@hotmail.com>
2024-07-26 17:06:58 -0300

    Initial setup

---
+++ form_newsletter/src/App.tsx
@@ -0,0 +1,14 @@
+
+import { Form } from './components/Form'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-red-600 text-3xl font-bold">Inscreva-se</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App
