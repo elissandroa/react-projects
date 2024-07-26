0b5232cfe7ba5e30efcd2c1f8f6ca12f257c6b24
elissandroa <elissandroa@hotmail.com>
2024-07-26 12:25:43 -0300

    Game logic

--- jogo_da_memoria/src/components/Board.jsx
+++ jogo_da_memoria/src/components/Board.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Card } from './Card'
 
-export const Board = ({cards}) => {
+export const Board = ({cards, onCardClick}) => {
     return (
         <div className='board'>
             {cards.map((card) => (
-                <Card key={card.id} card={card} />
+                <Card key={card.id} card={card} onClick={onCardClick} />
             ))}
         </div>
     )
--- jogo_da_memoria/src/components/Game.jsx
+++ jogo_da_memoria/src/components/Game.jsx
@@ -19,7 +19,7 @@ export const Game = () => {
             value,
             isFlipped: false,
         }));
-        const duplicateCards = cards.concat([...cards]).map((card, index) => ({ ...card, index }));
+        const duplicateCards = cards.concat([...cards]).map((card, index) => ({ ...card, id: index }));
         return shufleArray(duplicateCards);
 
     }
@@ -31,10 +31,41 @@ export const Game = () => {
 
     const result = cards.filter((card) => card.isFlipped).length;
 
+    const handleCardClick = (clickedCard) => {
+
+        if (chances === 0) return;
+        if (clickedCard === 2) return;
+
+        const newCards = cards.map((card) => {
+            return card.id === clickedCard.id ? { ...card, isFlipped: true } : card;
+        });
+
+        setCards(newCards);
+        setFlippedCards([...flippedCards, clickedCard]);
+
+        if (flippedCards.length === 1) {
+            setTimeout(() => {
+                const [firstCard] = flippedCards;
+                if (firstCard.value !== clickedCard) {
+                    const resetCards = cards.map((card) => {
+                      return card.id === firstCard.id || card.id === clickedCard.id
+                            ? { ...card, isFlipped: false }
+                            : card;
+                    })
+                    setCards(resetCards);
+                    setChances((prev) => prev - 1);
+
+                }
+
+                setFlippedCards([]);
+            }, 600);
+        }
+    }
+
     return (
         <div className='game'>
             <h3>Game</h3>
-            <Board cards={cards} />
+            <Board cards={cards} onCardClick={handleCardClick} />
             {chances === 0 ? (<p>Suas tentativas acabaram</p>) : (result === cards.length) ? (<h2>Parabéns você ganhou!</h2>) : (<p>você possui {chances} tentativa(s)</p>)}
             <button className='btn'>Reiniciar</button>
         </div>
