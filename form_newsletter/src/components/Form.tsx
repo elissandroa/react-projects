9fbe1cf8705edf3c40b4d0af767b1637d8419a97
elissandroa <elissandroa@hotmail.com>
2024-07-28 18:40:02 -0300

    Tailwind styles + Creating types and states

--- form_newsletter/src/components/Form.tsx
+++ form_newsletter/src/components/Form.tsx
@@ -1,22 +1,36 @@
+import { useState, FormEvent } from "react"
+import { User } from "../types/User"
+
+
 export const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agree, setAgree]  = useState(false);
+
+  const [errors, setErrors] = useState<User | null >(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  }
+
   return (
-    <form>
-      <div>
-        <label>Nome:</label>
-        <input type="text" placeholder="Digite o seu nome" />
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="name">Nome:</label>
+        <input type="text" placeholder="Digite o seu nome" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400" />
       </div>
-      <div>
-        <label>E-mail:</label>
-        <input type="email" placeholder="Insira seu melhor email" />
+      <div className="flex flex-col">
+        <label className="text-sm" htmlFor="email">E-mail:</label>
+        <input type="email" placeholder="Insira seu melhor email" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400" />
       </div>
-      <div>
-        <a href="#">Leia os termos</a>
-        <div>
+      <div className="flex flex-col">
+        <a href="#" className="text-xs underline mb-2">Leia os termos</a>
+        <div className="flex gap-2 item-center">
         <input type="checkbox"/>
-        <label>Concordo com os termos</label>
+        <label className="text-sm" htmlFor="agree">Concordo com os termos</label>
         </div>
       </div>
-      <button>Cadastrar</button>
+      <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
     </form>
   )
 }
